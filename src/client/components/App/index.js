439f4ba5a2692cb5e4e8d408e7bd048fccb587cc
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-03-26 09:13:59 +0100

    update structure and include scss modules

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -1,5 +1,8 @@
 import React from "react";
+import styles from "./App.scss";
 
-const App = () => <h1>React application served with Parcel.</h1>;
+const App = () => (
+  <h1 className={styles.app}>React application served with Parcel.</h1>
+);
 
 export default App;
